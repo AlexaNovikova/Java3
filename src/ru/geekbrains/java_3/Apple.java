f77a2fc5cecd297f10e6aea376576c803ee9eebb
AlexaNovikova <69957036+AlexaNovikova@users.noreply.github.com>
2020-12-05 14:23:36 +0300

    Домашняя работа к первому уроку Обобщения.

---
+++ src/ru/geekbrains/java_3/Apple.java
@@ -0,0 +1,8 @@
+package ru.geekbrains.java_3;
+
+public class Apple extends Fruits {
+
+    Apple() {
+        super(1.0f);
+    }
+}
---
+++ src/ru/geekbrains/java_3/Orange.java
@@ -0,0 +1,8 @@
+package ru.geekbrains.java_3;
+
+public class Orange extends Fruits {
+
+    Orange() {
+        super(1.5f);
+    }
+}
